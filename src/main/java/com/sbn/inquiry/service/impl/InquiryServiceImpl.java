1fb5ff2783d3bc011c891b09b673622ae5708cb5
ssm512 <shinsang512@naver.com>
2026-06-09 18:00:15 +0900

    2026_06_09_v1.0.1 env 환경변수 숨김

--- src/main/java/com/sbn/inquiry/service/impl/InquiryServiceImpl.java
+++ src/main/java/com/sbn/inquiry/service/impl/InquiryServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
 import com.sbn.inquiry.service.InquiryService;
 
 @Service
